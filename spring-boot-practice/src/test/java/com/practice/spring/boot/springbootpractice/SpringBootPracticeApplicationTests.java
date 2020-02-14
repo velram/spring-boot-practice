1789ecb41bfd5682207fc890a6fb399261023b2f
Velmurugan Moorthy <velmurugan1993@gmail.com>
2020-02-14 11:31:55 +0530

    Vel | SpringBoot practice initial commit

---
+++ spring-boot-practice/src/test/java/com/practice/spring/boot/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.boot.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
